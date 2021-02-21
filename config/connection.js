8eda11d641e6af4260332b48f6d0a77a90e56d95
Logan Morro <loganfmorro@gmail.com>
2021-02-21 11:23:55 -0500

    more updates

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "april4690",
+        password: process.env.PASS,
         database: "burgers_db"
     });
 }
