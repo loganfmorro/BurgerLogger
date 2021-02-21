2cdd05280942b8197842fd174b89744ae70aa83f
Logan Morro <loganfmorro@gmail.com>
2021-02-21 11:07:31 -0500

    debugging the javascript for burger.js

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: "epwjjqpo6xreg7l8",
-        password: "ux57a16kq50xuypg",
-        database: "novr3gmb0rs4zvdl"
+        host: "localhost",
+        user: "root",
+        password: "april4690",
+        database: "burgers_db"
     });
 }
 
